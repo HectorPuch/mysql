2dca26d7273adbc5f4a66083daea01f0fccebc82
Hector Puch <hectorpuchortonoves@gmail.com>
2024-11-16 22:36:46 +0100

    Exercise 03 Data Definition Language SQL

---
+++ exercise03_DDL.sql
@@ -0,0 +1,42 @@
+
+CREATE DATABASE IF NOT EXISTS db_escola;
+
+USE db_escola;
+
+CREATE TABLE IF NOT EXISTS alumnes (
+    NumMatricula VARCHAR(7) PRIMARY KEY,
+    NomAlumne VARCHAR(15),
+    DataNaixement DATE,
+    Telefon CHAR(9)
+);
+
+DESCRIBE alumnes;
+
+CREATE TABLE IF NOT EXISTS assignatures (
+    Codi VARCHAR(10) PRIMARY KEY,
+    NomAssignatura VARCHAR(15)
+);
+
+DESCRIBE assignatures;
+
+CREATE TABLE IF NOT EXISTS matriculacions (
+    IdMatriculacions INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    any_academic CHAR(9),
+    NumMatricula VARCHAR(7) NULL,
+    Codi VARCHAR(10) NULL,
+    FOREIGN KEY (NumMatricula) REFERENCES alumnes(NumMatricula) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (Codi) REFERENCES assignatures(Codi) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+DESCRIBE matriculacions;
+
+CREATE TABLE IF NOT EXISTS professors (
+    ID_P INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    NIF_P VARCHAR(9) UNIQUE,
+    Nom VARCHAR(15), 
+    Especialitat VARCHAR(10) NULL,
+    Telefon CHAR(9),
+    FOREIGN KEY (Especialitat) REFERENCES assignatures(Codi) ON DELETE SET NULL ON UPDATE CASCADE 
+);
+
+DESCRIBE professors;
