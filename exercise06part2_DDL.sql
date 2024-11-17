ed1f787eb0cdb4615b67e6797cf8f6d6c37d4875
Hector Puch <hectorpuchortonoves@gmail.com>
2024-11-17 09:55:36 +0100

    Exercise 06 part 2 Data Definition Language SQL

---
+++ exercise06part2_DDL.sql
@@ -0,0 +1,21 @@
+SHOW DATABASES;
+
+USE db_zoologics;
+
+SHOW TABLES;
+
+DESCRIBE zoologics;
+
+DESCRIBE animals;
+
+DESCRIBE especies;
+
+DROP TABLE especies;
+
+DROP TABLE animals;
+
+DROP TABLE zoologics;
+
+SHOW TABLES;
+
+SHOW DATABASES;
