e4f4e7fc502266b1bc236514237a6529adbc95ec
Hector Puch <hectorpuchortonoves@gmail.com>
2024-11-17 18:47:02 +0100

    Update Exercise 09 Data Definition Language SQL

--- exercise09_DDL.sql
+++ exercise09_DDL.sql
@@ -2,7 +2,12 @@ USE db_zoologics;
 
 SHOW CREATE TABLE animals;
 
+SHOW CREATE TABLE especies;
+
 ALTER TABLE animals DROP CONSTRAINT animals_ibfk_1;
-ALTER TABLE animals DROP CONSTRAINT animals_ibfk_2;
+
+ALTER TABLE especies DROP CONSTRAINT especies_ibfk_1;
 
 SHOW CREATE TABLE animals;
+
+SHOW CREATE TABLE especies;
