92fb062e0e77527edbe2bd957b10f7c2471fcdd3
Hector Puch <hectorpuchortonoves@gmail.com>
2024-11-17 09:26:09 +0100

    Exercise 05 part 2 Data Definition Language SQL

---
+++ exercise05part2_DDL.sql
@@ -0,0 +1,25 @@
+SHOW DATABASES;
+
+USE db_bufet_advocats;
+
+SHOW TABLES;
+
+DESCRIBE clients;
+
+DESCRIBE assumptes;
+
+DESCRIBE procuradors;
+
+DESCRIBE assumptes_procuradors;
+
+DROP TABLE assumptes_procuradors;
+
+DROP TABLE procuradors;
+
+DROP TABLE assumptes;
+
+DROP TABLE clients;
+
+SHOW TABLES;
+
+SHOW DATABASES;
