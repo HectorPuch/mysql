6afc47a30c64ad864fe869d2476d73088a7ad3ff
Hector Puch <hectorpuchortonoves@gmail.com>
2024-12-14 19:42:15 +0100

    Exercise 03 Data Manipulation Language Querys SQL

--- exercise03_DML_querys.sql
+++ exercise03_DML_querys.sql
@@ -14,3 +14,20 @@ SELECT ID_P FROM professors WHERE NIF_P LIKE '4%';
 
 SELECT Nom, Telefon FROM professors WHERE Telefon LIKE '%9';
 
+-- e) Extreure el num_matricula, nom i telefon dels alumnes que han nascut entre el 1995 i el 2005
+
+SELECT NumMatricula, NomAlumne, Telefon FROM alumnes WHERE DataNaixement BETWEEN '1995-01-01' AND '2005-12-31';
+
+-- f) Extreure el nom i el telèfon dels professors que imparteixen alguna assignatura
+
+SELECT professors.Nom, professors.Telefon FROM professors JOIN assignatures a ON professors.ID_P = assignatures.ID_P;
+
+-- g) Extreure el nom i any acadèmic de les assignatures en què està matriculat algun alumne
+
+SELECT assignatures.NomAssignatura, matriculacions.any_academic FROM assignatures JOIN matriculacions ON assignatures.Codi = matriculacions.Codi;
+
+-- h) Extreure el nom de cada assignatura i nombre total d'alumnes que estan matriculats en cadascuna
+
+SELECT assignatures.NomAssignatura, COUNT(matriculacions.NumMatricula) AS NombreAlumnes FROM assignatures LEFT JOIN matriculacions ON assignatures.Codi = matriculacions.Codi GROUP BY assignatures.Codi;
+
+
