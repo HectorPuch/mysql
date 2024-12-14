0518ab97016075d8602688d3be5d3bf3f6269c4e
Hector Puch <hectorpuchortonoves@gmail.com>
2024-12-14 19:51:19 +0100

    Exercise 04 Data Manipulation Language Querys SQL

--- exercise04_DML_querys.sql
+++ exercise04_DML_querys.sql
@@ -17,3 +17,19 @@ SELECT Nom, Telefon FROM empleats WHERE Salari < 20000;
 -- e) Extreure el nom i salari dels empleats que pertanyen a una localitat específica (per exemple, "Barcelona")
 
 SELECT empleats.Nom, empleats.Salari FROM empleats JOIN localitats ON empleats.CodiLocalitats = localitats.CodiLocalitats WHERE localitats.NomLocalitats = 'Barcelona';
+
+-- f) Extreure la mitjana de salari dels empleats que viuen a la localitat de 'Barcelona'
+
+SELECT AVG(empleats.Salari) AS MitjanaSalari FROM empleats JOIN localitats ON empleats.CodiLocalitats = localitats.CodiLocalitats WHERE localitats.NomLocalitats = 'Barcelona';
+
+-- g) Extreure el nom de totes les províncies que pertanyen a la regió "Comunitat Valenciana"
+
+SELECT provincies.NomProvincies FROM provincies JOIN regions ON provincies.NomRegions = regions.NomRegions WHERE regions.NomRegions = 'Comunitat Valenciana';
+
+-- h) Extreure el codi i nom de les localitats on no hi ha cap empleat assignat
+
+SELECT localitats.CodiLocalitats, localitats.NomLocalitats FROM localitats LEFT JOIN empleats ON localitats.CodiLocalitats = empleats.CodiLocalitats WHERE empleats.CodiLocalitats IS NULL;
+
+-- i) Extreure el nom i codi de les regions que tenen més d'una província associada
+
+SELECT regions.NomRegions, regions.NomRegions FROM regions JOIN provincies ON regions.NomRegions = provincies.NomRegions GROUP BY regions.NomRegions HAVING COUNT(provincies.CodiProvincies) > 1;
